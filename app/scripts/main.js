7ba227c7dd531c82bccfea6ad751c2b53d6ba2eb
Chris Price <chrisprice@rogers.com>
2018-10-31 10:43:37 -0400

    Additional debug output

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,13 @@
 var messages = require('protobuf/js/socket-gateway/SocketGatewayMessage_pb');
 
+function encode_utf8(s) {
+    return encodeURIComponent(s);
+}
+
+function decode_utf8(s) {
+    return decodeURIComponent(s);
+}
+
 var connection = new WebSocket('ws://kube-master:30808/open');
 connection.onopen = function()
 {
@@ -8,11 +16,25 @@ connection.onopen = function()
     message.setType(messages.RequestType.ECHO);
     message.setId(1);
     message.setData("test");
+
     console.log("Message: " + JSON.stringify(message.toObject()));
     var binary = message.serializeBinary();
     var str = "";
-    binary.forEach(function(elem) { str = str + String.fromCharCode(elem); });
-    connection.send(str);
+    var bytes = [];
+    binary.forEach(
+        function(elem)
+        {
+            bytes.push(elem);
+            str = str + String.fromCharCode(elem);
+            //encode_utf8(String.fromCharCode(elem));
+            console.log("Bytes so far: " + bytes);
+            console.log("encode_utf8(bytes): " + encode_utf8(bytes));
+            console.log("String so far: " + str);
+            console.log("encode_utf8(str): " + encode_utf8(str));
+        }
+    );
+    console.log("String: " + str + " encoded string: " + encode_utf8(str));
+    connection.send(encode_utf8(str));
 }
 
 connection.onmessage = function(message)
